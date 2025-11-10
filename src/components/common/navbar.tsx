a3b34c730b786cbe94ece971b0ca13c99cfafd22
muhammad-yeasin <76746216+muhammad-yeasin@users.noreply.github.com>
2025-11-10 14:15:08 +0600

    signout callback url changed

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -63,7 +63,7 @@ export default function Navbar() {
   const handleLogout = () => {
     signOut({
       redirect: true,
-      callbackUrl: "https://forumiq.muhammadyeasin.com",
+      callbackUrl: "/",
     });
   };
 
